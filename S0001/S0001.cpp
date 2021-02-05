93e9cb64af45051f09490bf6fae71edc2e6703b5
Tema Shevchenko <svev2369@gmail.com>
2021-02-05 12:24:58 +0700

    Добавьте файлы проекта.

---
+++ S0001/S0001.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    char s[256];
+    char a[] = "Привет, ";
+    char b[] = " Мир";
+    char f[] = "%s";
+
+    _asm
+    {
+        lea eax, s
+        push eax
+        lea ebx, a
+        push ebx
+        call scanf
+        add esp, 8
+
+    }
+}
+
+
