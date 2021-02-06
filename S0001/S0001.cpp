68d52ebf00031461a685de598e6aa48ba47b971c
Тема Шевченко <svev2369@gmail.com>
2021-02-06 10:52:37 +0700

    Верный код

--- S0001/S0001.cpp
+++ S0001/S0001.cpp
@@ -3,21 +3,33 @@
 
 int main()
 {
+   
     char s[256];
-    char a[] = "Привет, ";
-    char b[] = " Мир";
+    char a[] = "Hello, ";
+    char b[] = "World";
     char f[] = "%s";
-
     _asm  
     {
-        lea eax, s
-        push eax
-        lea ebx, a
-        push ebx
-        call scanf
-        add esp, 8
-        add esp, e
+        lea esi, [a]
+        lea edi, [s]
+
+        FOR_1:
+            lodsb
+            stosb
+            cmp al, 0
+        jnz FOR_1
+
+        lea esi, [b]
+        dec edi
+
+        FOR_2 :
+            lodsb
+            stosb
+            cmp al, 0
+        jnz FOR_2
+
     }
+    std::cout << s;
 }
 
 
